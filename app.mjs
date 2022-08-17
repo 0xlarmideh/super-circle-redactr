d8af6f79b41b225647c21b94014a2de6ae24f7b1
Abdulhameed Busari <65597134+0xlarmideh@users.noreply.github.com>
2022-08-17 20:55:23 +0000

    Reverted conflicting changes.

--- app.mjs
+++ app.mjs
@@ -1,10 +1,8 @@
 // DOM elements variables
 const resetBtn = document.querySelector("#reset-btn"),
   showRedact = document.querySelector(".show-result"),
-  rightSide = document.querySelector(".right-side"),
   submitBtn = document.querySelector("#submit-btn"),
   clearBtn = document.querySelector("#clear-btn"),
-  redactedDetails = document.querySelector(".redacted-details"),
   totalWords = document.querySelector("#total-words"),
   totalCharacters = document.querySelector("#total-characters"),
   totalRedacted = document.querySelector("#total-redacted"),
@@ -17,19 +15,25 @@ function startApp() {
 
 // function to redact text on click
 function getRedact(e) {
+  // getting the execution time
   let start = performance.now();
+  
+  // Input value variables
   const textContent = document.querySelector("#text-area").value,
     redactedWords = document.querySelector("#redacted-words").value,
     redactSymbol = document.querySelector("#redact-sym").value,
-    textContentSplit = textContent.split(/[\s]|[.,?;:"'-'"]/),
+    textContentSplit = textContent.split(/[\s]|[.,]/),
     totalTextChar = textContentSplit.join(""),
     redactedWordSplit = redactedWords.split(" "),
     totalRedactWords = [],
     newTextContentSplit = [];
-  for (let x of textContentSplit) {
-    newTextContentSplit.push(x.toLowerCase())
+
+  // Change redact words to lowercase
+  for (let word of textContentSplit) {
+    newTextContentSplit.push(word.toLowerCase())
   }
 
+  // Find a match and replace with desired symbol
   for (let item of redactedWordSplit) {
     if (newTextContentSplit.includes(item.toLowerCase())) {
       totalRedactWords.push(item)
@@ -37,25 +41,26 @@ function getRedact(e) {
       newTextContentSplit[index] = new String(convertSymbol(item, redactSymbol))
     }
   }
-  // console.log(newTextContentSplit)
+
+  // redaction results metrics
   showRedact.textContent = newTextContentSplit.join(" ");
   totalWords.textContent = textContentSplit.length;
   totalCharacters.textContent = totalTextChar.length;
   totalRedacted.textContent = totalRedactWords.length
-  // console.log(newTextContentSplit.length);
-
   let timeTaken = (performance.now() - start).toFixed(2);
   totalTime.textContent = timeTaken + "ms";
   console.log(timeTaken + "ms")
   e.preventDefault();
 }
 
+// function to clear all inputs
 function clearAll(e) {
   document.querySelector(".redact-form").reset();
   e.preventDefault();
 }
+
 // function to convert strings to symbols
-function convertSymbol(name, symbol = "@") {
+function convertSymbol(name, symbol) {
   let newItem = ""
   for (let index = 0; index < name.length; index++) {
     if (symbol == "") {
@@ -64,8 +69,6 @@ function convertSymbol(name, symbol = "@") {
     newItem += symbol
   } return newItem
 }
-
-// Function to count number of words scanned.
 
 // ======= DO NOT EDIT ============== //
 export default startApp;
